d34259cebef0161b8635d577b05e4aa29bd3bdf5
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-04-29 17:22:10 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import Home from "/home/reagan/Desktop/PHASE -2/react-hooks-import-export-lab/src/components/Home.js";
+import About from "/home/reagan/Desktop/PHASE -2/react-hooks-import-export-lab/src/components/About.js";
+import NavBar from "/home/reagan/Desktop/PHASE -2/react-hooks-import-export-lab/src/components/NavBar.js";
+
+// Use the components in your return() JSX
+
 function App() {
   return (
     <div>
